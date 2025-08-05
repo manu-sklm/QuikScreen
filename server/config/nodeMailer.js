7814c52dacaf7e1aad0d959b1558a84fb217961b
manu-sklm <manusklm74@gmail.com>
2025-08-05 17:17:16 +0530

    fidex bug:sntp not defined

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-  host:smtp-relay.brevo.com,
+  host:"smtp-relay.brevo.com",
   port: 587,
   auth: {
     user:process.env.SMTP_USER,
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
 const sendEmail= async ({to,subject,body}) => {
 
   const response = await transporter.sendMail({
-    from: process.env.SENDER_EMAIL,
+    from:process.env.SENDER_EMAIL,
     to,
     subject,
     html: body, // HTML body
